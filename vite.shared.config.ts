b5a1d4c781243afdbfe27b2cf58e4eda2e8dcbe4
Håkon Nessjøen <haakon.nessjoen@gmail.com>
2025-09-02 08:55:36 +0200

    add envDir so it reads .env from our monorepo root

--- vite.shared.config.ts
+++ vite.shared.config.ts
@@ -1,49 +1,51 @@
 /** @fileoverview The common config for frontend projects. */
 
-import { Rollup, UserConfig } from 'vite';
+import { Rollup, UserConfig } from "vite";
 
 export const manualChunks: Rollup.GetManualChunk = (id, { getModuleInfo }) => {
-  const hasReactDependency = (id: string): boolean => {
-    return getModuleInfo(id)
-      ?.importedIds
-      .some((importedId) =>
-        importedId.includes('react') ||
-        importedId.includes('react-dom')
-      ) ?? false;
-  }
+	const hasReactDependency = (id: string): boolean => {
+		return (
+			getModuleInfo(id)?.importedIds.some(
+				(importedId) =>
+					importedId.includes("react") || importedId.includes("react-dom"),
+			) ?? false
+		);
+	};
 
-  // Caution: React-related packages should be bundled together otherwise it will cause runtime errors
-  if (id.includes('/node_modules/') && hasReactDependency(id)) {
-    return 'react';
-  }
+	// Caution: React-related packages should be bundled together otherwise it will cause runtime errors
+	if (id.includes("/node_modules/") && hasReactDependency(id)) {
+		return "react";
+	}
 
-  if (id.includes('/@logto/')) {
-    return '@logto';
-  }
+	if (id.includes("/@logto/")) {
+		return "@logto";
+	}
 
-  if (id.includes('/node_modules/')) {
-    return 'vendors';
-  }
+	if (id.includes("/node_modules/")) {
+		return "vendors";
+	}
 
-  const match = /\/lib\/locales\/([^/]+)/.exec(id);
-  if (match?.[1]) {
-    return `phrases-${match[1]}`;
-  }
+	const match = /\/lib\/locales\/([^/]+)/.exec(id);
+	if (match?.[1]) {
+		return `phrases-${match[1]}`;
+	}
 };
 
 export const defaultConfig: UserConfig = {
-  resolve: {
-    alias: [
-      {
-        find: /^@\//,
-        replacement: '/src/',
-      },
-    ],
-  },
-  build: {
-    sourcemap: true,
-    rollupOptions: {
-      output: { manualChunks },
-    },
-  },
+	// Use project root for .env files
+	envDir: "../../",
+	resolve: {
+		alias: [
+			{
+				find: /^@\//,
+				replacement: "/src/",
+			},
+		],
+	},
+	build: {
+		sourcemap: true,
+		rollupOptions: {
+			output: { manualChunks },
+		},
+	},
 };
